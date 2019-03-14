a81b5f3ca4639004f23bc9b9ca309445fad292e9
Audrey Jones <audrey.eliz.jones@gmail.com>
2019-03-14 20:28:14 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -19,20 +19,12 @@ class Student
   def add_student_attributes(attributes_hash)
     attributes_hash.each do |key,value|
       attributes_hash[key]= value
-      binding.pry
+      # binding.pry
       # new_student.key = value
       # new_student[key] = value
     end
 
-                                                #     {:twitter=>"someone@twitter.com",
-                                                #  :linkedin=>"someone@linkedin.com",
-                                                #  :github=>"someone@github.com",
-                                                #  :blog=>"someone@blog.com",
-                                                #  :profile_quote=>
-                                                #   "\"Forget safety. Live where you fear to live. Destroy your reputation. Be notorious.\" - Rumi",
-                                                #  :bio=>
-                                                #   "I was in southern California for college (sun and In-n-Out!), rural Oregon for high school (lived in a town with 1500 people and 3000+ cows), and Tokyo for elementary
-                                                # /middle school."}
+                                                
     new_student = Student.new(attributes_hash)
     attributes_hash
   end
