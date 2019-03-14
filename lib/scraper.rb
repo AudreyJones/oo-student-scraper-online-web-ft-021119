9626112ebe0c55d0b766e815b6e413444e25dd00
Audrey Jones <audrey.eliz.jones@gmail.com>
2019-03-14 04:00:49 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,21 @@
 require 'open-uri'
+require 'nokogiri'
 require 'pry'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    html = open(index_url)
+    doc = Nokogiri::HTML(html)
+# Student Name: doc.css("h4").first.text
+# Location: doc.css("p").first.text
+#Link to Page: doc.css("a")[1].attr("href")
+
+    binding.pry
   end
 
   def self.scrape_profile_page(profile_url)
-    
+
   end
 
 end
-
