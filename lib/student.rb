38c1f2103f671d70a6f070e62df74708ca217dc0
Audrey Jones <audrey.eliz.jones@gmail.com>
2019-03-14 20:11:01 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -11,6 +11,7 @@ class Student
   end
 
   def self.create_from_collection(students_array)
+    binding.pry
                                                           #[{:name=>"Alex Patriquin", :location=>"New York, NY"},
                                                            # {:name=>"Bacon McRib", :location=>"Kansas City, MO"},
                                                            # {:name=>"Alisha McWilliams", :location=>"San Francisco, CA"},
