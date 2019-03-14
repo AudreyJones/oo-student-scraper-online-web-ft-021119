f4c3bab3df315abe775d70986804cd476d5307eb
Audrey Jones <audrey.eliz.jones@gmail.com>
2019-03-14 20:08:56 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -10,8 +10,15 @@ class Student
     @@all << self
   end
 
-  def self.create_from_collection(students_array)
-    binding.pry
+  def self.create_from_collection(students_array) 
+                                                          #[{:name=>"Alex Patriquin", :location=>"New York, NY"},
+                                                           # {:name=>"Bacon McRib", :location=>"Kansas City, MO"},
+                                                           # {:name=>"Alisha McWilliams", :location=>"San Francisco, CA"},
+                                                           # {:name=>"Daniel Fenjves", :location=>"Austin, TX"},
+                                                           # {:name=>"Arielle Sullivan", :location=>"Chicago, IL"},
+                                                           # {:name=>"Sushanth Bhaskarab", :location=>"Portland, OR"},
+                                                           # {:name=>"Sushanth Bhaskarab", :location=>"Portland, OR"}]
+    
   end
 
   def add_student_attributes(attributes_hash)
