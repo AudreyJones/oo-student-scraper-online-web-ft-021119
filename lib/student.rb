0cb7a4b125ea4760516f4d81e15dc4abd3854bd0
Audrey Jones <audrey.eliz.jones@gmail.com>
2019-03-14 20:12:09 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -12,7 +12,7 @@ class Student
 
   def self.create_from_collection(students_array)
     students_array.each do |name, location|
-      
+
     end
     binding.pry
                                                           #[{:name=>"Alex Patriquin", :location=>"New York, NY"},
