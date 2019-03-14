5fe677b6aaf50a5cfcc853fb341c240d5d2f088c
Audrey Jones <audrey.eliz.jones@gmail.com>
2019-03-14 14:05:17 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,11 +7,25 @@ class Scraper
   def self.scrape_index_page(index_url)
     html = open(index_url)
     doc = Nokogiri::HTML(html)
+    students = {}
+
 # Student Name: doc.css("h4").first.text
 # Location: doc.css("p").first.text
 #Link to Page: doc.css("a")[1].attr("href")
+# Collection: doc.css(".roster-cards-container")
 
-    binding.pry
+# Expected return: an array of hashes in which each hash represents one student
+# Iterate through the students:
+    doc.css(".roster-cards-container").each do |student|
+          # student = Student.new
+          students = {
+            :name => doc.css("h4").text
+           :location => doc.css("p").first.text
+           :profile_url => doc.css("a")[1].attr("href")
+          }
+
+        end
+    # {:name => doc.css("h4").text, :location => doc.css("p").text, :profile_url => doc.css("a")[1].attr("href")}
   end
 
   def self.scrape_profile_page(profile_url)
